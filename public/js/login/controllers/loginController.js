088f39600a342eb31f9c70e659881cd7c764a7a7
brandonmichaelhunter <brandonmichaelhunter@live.com>
2017-01-05 22:10:24 -0500

    Add a home page and a new screen for adding a new recruit.

--- public/js/login/controllers/loginController.js
+++ public/js/login/controllers/loginController.js
@@ -1,1 +1,28 @@
-﻿;
+﻿app.controller('loginController', function ($scope) {
+    $scope.message = 'Firebase Demo App';
+    var config = {
+        apiKey: "AIzaSyD4I-ZbYU-RsinBKZA8gPdEjcWP1oVaG6c",
+        authDomain: "statesportsrecruitingdb.firebaseapp.com",
+        databaseURL: "https://statesportsrecruitingdb.firebaseio.com",
+        storageBucket: "statesportsrecruitingdb.appspot.com",
+        messagingSenderId: "1098944814860"
+    };
+    firebase.initializeApp(config);
+    // FirebaseUI config.
+    var uiConfig = {
+        signInSuccessUrl: 'pages/home.html',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+});
---
+++ public/js/recurits/controllers/recruitController.js
@@ -0,0 +1,12 @@
+﻿app.controller('recruitController', function ($scope) {
+    $scope.message = 'Add new recruit';
+    var config = {
+        apiKey: "AIzaSyD4I-ZbYU-RsinBKZA8gPdEjcWP1oVaG6c",
+        authDomain: "statesportsrecruitingdb.firebaseapp.com",
+        databaseURL: "https://statesportsrecruitingdb.firebaseio.com",
+        storageBucket: "statesportsrecruitingdb.appspot.com",
+        messagingSenderId: "1098944814860"
+    };
+    firebase.initializeApp(config);
+    
+});
